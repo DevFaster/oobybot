7a410ef6e5bbf1836c6bf1313e86d5235e261810
DevFaster <73589318+DevFaster@users.noreply.github.com>
2023-07-20 11:49:24 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,14 @@ const wheelRadius = 2 // cm
 let blink = false
 let blinkDelay = 1000
 
+enum Version {
+    //% block="servomoteurs"
+    Servo,
+    //% block="moteurs CC"
+    DCMotor,
+    Undefined
+}
+
 enum Movement {
     //% block="avancer"
     Forward = 1,
@@ -41,6 +49,9 @@ enum DistanceUnit {
     INCH
 }
 
+let version = Version.Undefined
+let error = false
+
 function blinkLED() {
     while (blink) {
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -57,11 +68,24 @@ basic.forever(blinkLED)
  */
 //% weight=100 color=#af5200 icon=""
 namespace Oobybot {
+    function checkInit(): void {
+        if (!error) {
+            if (version == Version.Undefined) {
+                error = true
+                control.reset()
+            }
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    }
+
     /**
      * Initialiser le robot Oobybot et ses capteurs
+     * @param version La version du robot (avec servomoteurs ou moteurs CC)
      */
     //% block="initialiser l'Oobybot"
-    export function init(): void {
+    export function init(_version: Version): void {
+        version = _version
     }
 
     /**
@@ -94,81 +118,37 @@ namespace Oobybot {
     export function ledStop(): void {
         blink = false
     }
-
-    /**
-     * Permet de contrôler la direction (avant / arrière) ainsi que la vitesse (de 0 à 100%) des servomoteurs continus du robot Oobybot
-     * @param direction La direction du mouvement
-     * @param speed La vitesse des moteurs
-     */
-    //% block="[Servo] contrôler le robot $direction vitesse $speed %"
-    //% subcategory="Servomoteurs"
-    //% group="Mouvement basique"
-    export function servoMove(direction: Movement, speed: number): void {
+    
+    function servoMove(direction: Movement, speed: number): void {
         pins.servoWritePin(AnalogPin.P1, 90 + direction * speed * 0.9)
         pins.servoWritePin(AnalogPin.P2, 90 - direction * speed * 0.9)
     }
 
-    /**
-     * Permet de contrôler la direction (droite / gauche) ainsi que la vitesse (de 0 à 100%) des servomoteurs continus du robot Oobybot
-     * @param side La direction de rotation
-     * @param speed La vitesse des moteurs
-     */
-    //% block="[Servo] pivoter vers la %side vitesse %speed %"
-    //% subcategory="Servomoteurs"
-    //% group="Mouvement basique"
-    export function servoRotate(side: Side, speed: number): void {
+    function servoRotate(side: Side, speed: number): void {
         pins.servoWritePin(AnalogPin.P1, 90 + side * speed * 0.9)
-        pins.servoWritePin(AnalogPin.P2, 90 - side * speed * 0.9)
+        pins.servoWritePin(AnalogPin.P2, 90 + side * speed * 0.9)
     }
 
-    /**
-     * Permet de tourner le robot sur lui même d'un certain angle selon une direction (droite / gauche)
-     * @param side La direction de rotation
-     * @param angle L'angle de rotation
-     */
-    //% block="[Servo] tourner à $side de $angle °"
-    //% subcategory="Servomoteurs"
-    //% group="Mouvement basique"
-    export function servoRotateAngle(side: Side, angle: number): void {
+    function servoRotateAngle(side: Side, angle: number): void {
         servoRotate(side, 100)
         basic.pause(angle * wheelDistance / (360 * wheelRadius * servoSpeed))
         servoStop()
     }
 
-    /**
-     * Permet d'arrêter la course des servomoteurs continus du robot Oobybot
-     */
-    //% block="[Servo] arrêter la course du robot"
-    //% subcategory="Servomoteurs"
-    //% group="Mouvement basique"
     export function servoStop(): void {
         pins.servoWritePin(AnalogPin.P1, 90)
         pins.servoWritePin(AnalogPin.P2, 90)
     }
 
-    /**
-     * Permet de contrôler un servomoteur continu (droit / gauche), sa direction (avant / arrière) et sa vitesse (de 0 à 100%) du robot Oobybot
-     */
-    //% block="[Servo] contrôler le servo $side direction $direction vitesse $speed %"
-    //% subcategory="Servomoteurs"
-    //% group="Mouvement avancé"
-    export function servoControl(side: Side, direction: Movement, speed: number): void {
+    function servoControl(side: Side, direction: Movement, speed: number): void {
         if (side == Side.Right) {
             pins.servoWritePin(AnalogPin.P1, 90 + direction * speed * 0.9)
         } else {
             pins.servoWritePin(AnalogPin.P2, 90 - direction * speed * 0.9)
         }
     }
-
-    /**
-     * Permet de contrôler la direction (avant / arrière) ainsi que la vitesse (de 0 à 100%) des moteurs à courant continu du robot Oobybot
-     * @param direction La direction du mouvement
-     * @param speed La vitesse des moteurs
-     */
-    //% block="[MCC] contrôler le robot $direction vitesse $speed %"
-    //% subcategory="Moteurs CC"
-    //% group="Mouvement basique"
-    export function dcMove(direction: Movement, speed: number): void {
+    
+    function dcMove(direction: Movement, speed: number): void {
         if (direction == Movement.Forward) {
             pins.analogWritePin(AnalogPin.P1, speed * 10.23)
             pins.analogWritePin(AnalogPin.P4, speed * 10.23)
@@ -178,15 +158,7 @@ namespace Oobybot {
         }
     }
 
-    /**
-     * Permet de contrôler la direction (droite / gauche) ainsi que la vitesse (de 0 à 100%) des moteurs à courant continu du robot Oobybot
-     * @param side La direction de rotation
-     * @param speed La vitesse des moteurs
-     */
-    //% block="[MCC] pivoter vers la %side vitesse %speed %"
-    //% subcategory="Moteurs CC"
-    //% group="Mouvement basique"
-    export function dcRotate(side: Side, speed: number): void {
+    function dcRotate(side: Side, speed: number): void {
         if (side == Side.Left) {
             pins.analogWritePin(AnalogPin.P1, speed * 10.23)
             pins.analogWritePin(AnalogPin.P2, speed * 10.23)
@@ -196,37 +168,17 @@ namespace Oobybot {
         }
     }
 
-    /**
-     * Permet de tourner le robot sur lui même d'un certain angle selon une direction (droite / gauche)
-     * @param side La direction de rotation
-     * @param angle L'angle de rotation
-     */
-    //% block="[MCC] tourner à $side de $angle °"
-    //% subcategory="Moteurs CC"
-    //% group="Mouvement basique"
-    export function dcRotateAngle(side: Side, angle: number): void {
+    function dcRotateAngle(side: Side, angle: number): void {
         dcRotate(side, 100)
         basic.pause(angle * wheelDistance / (360 * wheelRadius * dcSpeed))
         dcStop()
     }
 
-    /**
-     * Permet d'arrêter la course des moteurs à courant continu du robot Oobybot
-     */
-    //% block="[MCC] arrêter la course du robot"
-    //% subcategory="Moteurs CC"
-    //% group="Mouvement basique"
-    export function dcStop(): void {
+    function dcStop(): void {
         pins.analogWritePin(AnalogPin.P1, 0)
         pins.analogWritePin(AnalogPin.P4, 0)
     }
 
-    /**
-     * Permet de contrôler un moteur à courant continu (droit / gauche), sa direction (avant / arrière) et sa vitesse (de 0 à 100%) du robot Oobybot
-     */
-    //% block="[MCC] contrôler le moteur $side direction $direction vitesse $speed %"
-    //% subcategory="Moteurs CC"
-    //% group="Mouvement avancé"
     export function dcControl(side: Side, direction: Movement, speed: number): void {
         if (side == Side.Right) {
             if (direction == Movement.Forward) {
@@ -240,6 +192,90 @@ namespace Oobybot {
             } else {
                 pins.analogWritePin(AnalogPin.P4, speed * 10.23)
             }
+        }
+    }
+
+    /**
+     * Permet de contrôler la direction (avant / arrière) ainsi que la vitesse (de 0 à 100%) du robot Oobybot
+     * @param direction La direction du mouvement
+     * @param speed La vitesse des moteurs
+     */
+    //% block="[Servo] contrôler le robot $direction vitesse $speed %"
+    //% subcategory="Mouvement"
+    //% group="Mouvement basique"
+    export function move(direction: Movement, speed: number): void {
+        checkInit()
+        if (version == Version.Servo) {
+            servoMove(direction, speed)
+        } else {
+            dcMove(direction, speed)
+        }
+    }
+
+    /**
+     * Permet de contrôler la direction (droite / gauche) ainsi que la vitesse (de 0 à 100%) du robot Oobybot
+     * @param side La direction de rotation
+     * @param speed La vitesse des moteurs
+     */
+    //% block="[Servo] pivoter vers la %side vitesse %speed %"
+    //% subcategory="Mouvement"
+    //% group="Mouvement basique"
+    export function rotate(side: Side, speed: number): void {
+        checkInit()
+        if (version == Version.Servo) {
+            servoRotate(side, speed)
+        } else {
+            dcRotate(side, speed)
+        }
+    }
+
+    /**
+     * Permet de tourner le robot sur lui même d'un certain angle selon une direction (droite / gauche)
+     * @param side La direction de rotation
+     * @param angle L'angle de rotation
+     */
+    //% block="[Servo] tourner à $side de $angle °"
+    //% subcategory="Mouvement"
+    //% group="Mouvement basique"
+    export function rotateAngle(side: Side, angle: number): void {
+        checkInit()
+        if (version == Version.Servo) {
+            servoRotateAngle(side, angle)
+        } else {
+            dcRotateAngle(side, angle)
+        }
+    }
+
+    /**
+     * Permet d'arrêter la course du robot Oobybot
+     */
+    //% block="[Servo] arrêter la course du robot"
+    //% subcategory="Mouvement"
+    //% group="Mouvement basique"
+    export function moveStop(): void {
+        checkInit()
+        if (version == Version.Servo) {
+            servoStop()
+        } else {
+            dcStop()
+        }
+    }
+
+    /**
+     * Permet de contrôler un moteur (droit / gauche), sa direction (avant / arrière) et sa vitesse (de 0 à 100%) du robot Oobybot
+     * @param side Le moteur droit ou gauche
+     * @param  direction Le sens de rotation du moteur (avant / arrière)
+     * @param speed La vitesse de rotation du moteur
+     */
+    //% block="[Servo] contrôler le moteur $side direction $direction vitesse $speed %"
+    //% subcategory="Mouvement"
+    //% group="Mouvement avancé"
+    export function moveControl(side: Side, direction: Movement, speed: number): void {
+        checkInit()
+        if (version == Version.Servo) {
+            servoControl(side, direction, speed)
+        } else {
+            dcControl(side, direction, speed)
         }
     }
 
